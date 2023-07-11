6c747095947a0c0b6e07c09830fd9a98b6c43e07
pepopg <136018776+pepopg@users.noreply.github.com>
2023-07-11 18:49:38 +0300

    Update DesktopLauncher.java

--- desktop/src/com/droplet/game/DesktopLauncher.java
+++ desktop/src/com/droplet/game/DesktopLauncher.java
@@ -1,6 +1,5 @@
 package com.droplet.game;
 
-import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
